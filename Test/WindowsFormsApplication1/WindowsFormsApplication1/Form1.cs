b02f625c724a33dc993259424110f7f2641742e1
fs11709394 <fs11709394@126.com>
2014-02-20 15:35:15 +0800

    2014-02-20 15:35

--- Test/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ Test/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -18,7 +18,8 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
+            var msg = "Test, Ha ha.";
+            MessageBox.Show(msg);
         }
     }
 }
